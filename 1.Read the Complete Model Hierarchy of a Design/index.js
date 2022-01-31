5a6464d791c0184affe4d7738ebfccaeb603248d
Adam Nagy <adam_e_nagy@hotmail.com>
2022-01-31 13:54:38 +0000

    small update

--- 1.Read the Complete Model Hierarchy of a Design/index.js
+++ 1.Read the Complete Model Hierarchy of a Design/index.js
@@ -1,14 +1,14 @@
 import MyApp from './app.js'; 
 import MyAuth from './auth.js';
 
-// Replace the string literal values with your own client ID, client secret, 
-// hub ID, project ID and item version ID. 
+// Replace the string literal values with your own client id, client secret, 
+// hub name, project name and file name. 
 
-const clientId = '8kAAlxHgPEIYLQuYSc0EuEAL0gqYQVkv';
-const clientSecret = 'EuYAvTd9UsXDaNYu';
-const hubName = 'tapnair-staging';
-const projectName = 'Testing';
-const fileName = 'shapes';
+const clientId = '<YOUR_CLIENT_ID>';
+const clientSecret = '<YOUR_CLIENT_SECRET>';
+const hubName = '<YOUR_HUB_NAME>';
+const projectName = '<YOUR_PROJECT_NAME>';
+const fileName = '<YOUR_FILE_NAME>';
 
 // Create an instance of auth.js.
 let myForgeAuth = new MyAuth(clientId, clientSecret);
